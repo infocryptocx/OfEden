3c276837760ee42d357431f8e54a099ce393e19e
NF Devs <nonfungibledevs@gmail.com>
2022-07-13 16:47:45 -0700

    fifth commit

--- client/components/MainLayout.js
+++ client/components/MainLayout.js
@@ -2,10 +2,11 @@ import React, { Children } from "react";
 import Footer from "./Footer";
 import MainNavbar from "./MainNavbar";
 
+
 const MainLayout = ({ children }) => {
   return (
     <div className="bg-zinc-800">
-      <MainNavbar />
+      <MainNavbar /> 
       {children}
       <Footer />
     </div>
--- client/components/MainNavbar.js
+++ client/components/MainNavbar.js
@@ -9,6 +9,8 @@ const MainNavbar = () => {
   const { currentUser, currentAccount, connectWallet } =
     useContext(CryptoWorldContext);
 
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
     <nav className="shadow bg-black">
       <div className="container px-6 py-3 mx-auto">
@@ -29,27 +31,22 @@ const MainNavbar = () => {
               </Link>
             </div>
 
-            {currentAccount ? (
-              <div className="flex md:hidden">
-                <button
-                  type="button"
-                  className="text-gray-500  hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
-                  aria-label="toggle menu"
-                >
-                  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+            <button
+                className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                type="button"
+                onClick={() => setNavbarOpen(!navbarOpen)}
+              >
+                <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
                     <path
                       fillRule="evenodd"
                       d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
                     ></path>
                   </svg>
-                </button>
-              </div>
-            ) : (
-              <div></div>
-            )}
+              </button>
           </div>
 
-          <div className="items-center md:flex">
+          <div className={"items-center md:flex" +
+                (navbarOpen ? " flex" : " hidden")}>
             {currentAccount ? (
               <div className="flex flex-col md:flex-row">
                 <Link href="/">
@@ -158,7 +155,7 @@ const MainNavbar = () => {
                 <></>
               ) : (
                 <button
-                  className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-red-900 rounded-md hover:bg-zinc-600 md:mx-2 md:w-auto"
+                  className="block w-full px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-red-900 rounded-md hover:bg-zinc-600 md:mx-2 md:w-auto"
                   onClick={() => connectWallet()}
                 >
                   Connect Wallet
