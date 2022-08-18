f19cf04f5a5127334dfe27f2dc5d1c1de63d018f
NF Devs <nonfungibledevs@gmail.com>
2022-08-17 20:03:15 -0700

    download video option

--- client/pages/allaudios.js
+++ client/pages/allaudios.js
@@ -99,7 +99,7 @@ const AllAudios = ({ audios }) => {
 
                 <ReactAudioPlayer
                   src={getFileAsset(audio.audio, client.config()).url}
-                  controls
+                  controlsList="nodownload"
                 />
                 <div className="mt-4">
                   <div className="flex items-center">
